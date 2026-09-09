0c009699a6a052574c31a29e628f1d54403a847f
Lucas Brun <lucbrun43000@gmail.com>
2026-09-09 15:30:15 +0200

    init du SFML

---
+++ RTSproject/RTSproject.cpp
@@ -0,0 +1,30 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(
+        sf::VideoMode({ 800, 600 }),
+        "Simulation proie-predateur"
+    );
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+
+        sf::CircleShape proie(10.f);
+        proie.setFillColor(sf::Color::Green);
+        proie.setPosition({ 100.f, 100.f });
+
+        window.draw(proie);
+
+        window.display();
+    }
+
+    return 0;
+}
