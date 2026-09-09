b8914f8f4ca1cd9fc7da45a97ad18c324b1b540c
lucasbies <lucasbieszczad@gmail.com>
2026-09-09 16:41:02 +0200

    ajout d'un fichiers de variable globale et de certane de variable globale

---
+++ RTSproject/Variable.cpp
@@ -0,0 +1,11 @@
+unsigned int t = 1; // unité de temps en jour
+
+//Proies
+unsigned int popInitialeH = 2; // population initiale de proies
+double tauxReproductionH = 0.05; // taux de reproduction des proies
+double tauxMortaliteH = 0.01; // taux de mortalité des proies
+
+//Prédateurs
+unsigned int popInitialeC = 1; // population initiale de prédateurs
+double tauxReproductionC = 0.02; // taux de reproduction des prédateurs
+double tauxMortaliteC = 0.05; // taux de mortalité des prédateurs
