bcfe07446120319adde9d32c8be30e26f7ed8b13
lucasbies <lucasbieszczad@gmail.com>
2026-09-14 11:02:27 +0200

    ajout de la grille sur le paysage

--- RTSproject/RTSproject.cpp
+++ RTSproject/RTSproject.cpp
@@ -1,40 +1,75 @@
 ﻿#include <SFML/Graphics.hpp>
+#include "Variable.cpp"
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(
-        sf::VideoMode({ 800, 600 }),
-        "RTS"
-    );
+	//creation de la fenêtre
+	sf::RenderWindow window(
+		sf::VideoMode({ windowWidth, windowHeight }),
+		"RTS"
+	);
 
-    const sf::Texture texture("paysage.jpg");
-    sf::Sprite sprite(texture);
+	sf::Vector2u windowSize = window.getSize();
 
-    sprite.setScale({
-        static_cast<float>(window.getSize().x) / texture.getSize().x,
-        static_cast<float>(window.getSize().y) / texture.getSize().y
-        });
+	//chargement de l'image
+	const sf::Texture texture("paysage.jpg");
+	sf::Sprite sprite(texture);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+	//redimensionnement de l'image pour qu'elle remplisse la fenêtre
+	sprite.setScale({
+		static_cast<float>(windowSize.x) / texture.getSize().x,
+		static_cast<float>(windowSize.y) / texture.getSize().y
+		});
 
-        window.clear();
+	//calcule du nombre de ligne pour couvrir la fenetre et du nombre de vertex pour la grille
+	unsigned int numLinesX = (windowSize.x / cellSize) + 1;
+	unsigned int numLinesY = (windowSize.y / cellSize) + 1;
+	unsigned int totalVertices = (numLinesX + numLinesY) * 2;
 
-        sf::CircleShape proie(10.f);
-        proie.setFillColor(sf::Color::Green);
-        proie.setPosition({ 100.f, 100.f });
+	//création d'un tableau de vertex pour la grille
+	sf::VertexArray grid(sf::PrimitiveType::Lines, totalVertices);
+	size_t vertexIndex = 0;
 
-        window.draw(sprite);
+	//génération des lignes verticales
+	for (size_t i = 0; i < numLinesX; ++i)
+	{
+		float posX = i * cellSize;
+		grid[vertexIndex++] = sf::Vertex{ { posX, 0.f }, gridColor };
+		grid[vertexIndex++] = sf::Vertex{ { posX, float(windowSize.y) }, gridColor };
+	}
 
-        window.draw(proie);
+	//génération des lignes horizontales
+	for (size_t j = 0; j < numLinesY; ++j)
+	{
+		float posY = j * cellSize;
+		grid[vertexIndex++] = sf::Vertex{ { 0.f, posY }, gridColor };
+		grid[vertexIndex++] = sf::Vertex{ { float(windowSize.x), posY }, gridColor };
+	}
 
-        window.display();
-    }
+	while (window.isOpen())
+	{
+		//gestion des événements
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
 
-    return 0;
+		window.clear();
+
+		sf::CircleShape proie(10.f);
+		proie.setFillColor(sf::Color::Green);
+		proie.setPosition({ 100.f, 100.f });
+
+		window.draw(sprite);
+
+		window.draw(grid);
+
+		window.draw(proie);
+
+		window.display();
+	}
+
+	return 0;
 }
--- RTSproject/Variable.cpp
+++ RTSproject/Variable.cpp
@@ -1,4 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+
 unsigned int t = 1; // unité de temps en jour
+unsigned int windowWidth = 1600; // Largeur de la fenêtre
+unsigned int windowHeight = 900; // Hauteur de la fenêtre
+
+//Grille
+float cellSize = 50.f; // Taille des cellules de la grille
+sf::Color gridColor = sf::Color(255, 255, 255); // Couleur des lignes de la grille
+
 
 //Proies
 unsigned int popInitialeH = 2; // population initiale de proies
@@ -9,3 +20,5 @@ double tauxMortaliteH = 0.01; // taux de mortalité des proies
 unsigned int popInitialeC = 1; // population initiale de prédateurs
 double tauxReproductionC = 0.02; // taux de reproduction des prédateurs
 double tauxMortaliteC = 0.05; // taux de mortalité des prédateurs
+
+
