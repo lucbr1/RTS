85a84a80738186e09690cdfc22240afaab14acbd
Lucas Brun <lucbrun43000@gmail.com>
2026-09-21 11:38:17 +0200

    Implémentation du Lotka Volterra semi implicite

--- AlgosRTS/LotkaVolterra/LotkaVolterra.cpp
+++ AlgosRTS/LotkaVolterra/LotkaVolterra.cpp
@@ -17,7 +17,21 @@ std::ofstream fichier; // fichier pour stocker les résultats
 std::tuple<double, double> CalculerPopulations(double Hn, double Cn, double& alpha, double& beta, double& gamma, double& delta, unsigned int& dt)
 {
 	double H = Hn * (dt * (alpha - beta * Cn) + 1);
+	if (H < 0.0) H = 0.0; // Empêche l'oscillation
+
 	double C = Cn * (dt * (-gamma + delta * Hn) + 1);
+	if (C < 0.0) C = 0.0; // Empêche l'oscillation)
+
+	return std::make_tuple(H, C);
+}
+
+std::tuple<double, double> CalculerPopulationsSemiImplicite(double Hn, double Cn, double& alpha, double& beta, double& gamma, double& delta, unsigned int& dt)
+{
+	double H = Hn * (1 + dt * (alpha - beta * Cn));
+	if (H < 0.0) H = 0.0; // Empêche l'oscillation
+
+	double C = Cn * (1 + dt * (delta * H - gamma));
+	if (C < 0.0) C = 0.0; // Empêche l'oscillation
 
 	return std::make_tuple(H, C);
 }
@@ -26,7 +40,11 @@ std::tuple<double, double> calculerEvolutionPopulations(double Hn, double Cn, do
 {
 	for (unsigned int i = 1; i < n; i++)
 	{
-		std::tie(Hn, Cn) = CalculerPopulations(Hn, Cn, alpha, beta, gamma, delta, dt);
+		/// ------------------------------------------------
+		/// Choisir entre implicite et explicite en commentant/décommentant les lignes suivantes
+		/// ------------------------------------------------
+		std::tie(Hn, Cn) = CalculerPopulationsSemiImplicite(Hn, Cn, alpha, beta, gamma, delta, dt);
+		// std::tie(Hn, Cn) = CalculerPopulations(Hn, Cn, alpha, beta, gamma, delta, dt);
 		std::cout << "Population au jour " << i * dt << " : " << Hn << ", " << Cn << std::endl;
 
 		fichier << i * dt << ";" << Hn << ";" << Cn << ";\n";
